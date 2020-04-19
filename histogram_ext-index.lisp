ab7e126d30bbcc257d9e9e5d1159ed966ee9d51f
Robert Dodier <robert_dodier@users.sourceforge.net>
2020-04-19 14:22:55 -0700

    Commit Texinfo output file and documentation index, as generated by:
      $ makeinfo histogram_ext.texi
      $ perl $MAXIMA_SRCDIR/doc/info/build_index.pl histogram_ext.info :utf8 > histogram_ext-index.lisp
    
    where $MAXIMA_SRCDIR is the top directory of a Git repo or unpacked tarball.
    
    Note that the ':utf8' argument is needed to enable build_index.pl to
    handle UTF-8 characters in the .info file (originating from UTF-8 in
    the .texi).
    
    In a Maxima session, load("histogram_ext-index.lisp") loads the documentation index.
    Then ? and ?? find the documentation items, e.g.: ?? bubble

---
+++ histogram_ext-index.lisp
@@ -0,0 +1,14 @@
+(in-package :cl-info)
+(let (
+(deffn-defvr-pairs '(
+; CONTENT: (<INDEX TOPIC> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS> <NODE NAME>))
+("bubbles" . ("histogram_ext.info" 7534 3661 "Definitions for histogram_ext"))
+("histogram_bubbles" . ("histogram_ext.info" 6468 1057 "Definitions for histogram_ext"))
+("histogram_draw" . ("histogram_ext.info" 5600 863 "Definitions for histogram_ext"))
+("histogram_ext" . ("histogram_ext.info" 1996 3598 "Definitions for histogram_ext"))
+))
+(section-pairs '(
+; CONTENT: (<NODE NAME> . (<FILENAME> <BYTE OFFSET> <LENGTH IN CHARACTERS>))
+("Definitions for histogram_ext" . ("histogram_ext.info" 1928 9250))
+)))
+(load-info-hashtables (maxima::maxima-load-pathname-directory) deffn-defvr-pairs section-pairs))
